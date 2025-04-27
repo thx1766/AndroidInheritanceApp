4bc6ed6b49845993d3afa886939036113e397b1b
Nate Schaffner <nathanial.schaffner@gmail.com>
2025-04-27 07:07:27 -0400

    Lesson 129 - Day 8 - Functions that didn't exist and could be called (Inheritance example)

--- app/src/main/java/dev/thx1766/androidinheritanceapp/BaseClass.kt
+++ app/src/main/java/dev/thx1766/androidinheritanceapp/BaseClass.kt
@@ -3,7 +3,7 @@ package dev.thx1766.androidinheritanceapp
 open class BaseClass {
 
     fun coreValues(){
-        println("Core values of house baseclass")
+        println("Core values of house BaseClass")
     }
 
 }
